91c5e8d5955fdeb8e7353c193ccb841df500392a
LoserPanda <loserpanda82@gmail.com>
2018-08-31 11:08:25 +0300

    joujou

--- src/SignUp/index.js
+++ src/SignUp/index.js
@@ -4,6 +4,27 @@ import { withRouter } from "react-router";
 import app from "../base";
 
 class SignUpContainer extends Component {
+
+  postUserToDB = (UID, email) => {
+    const url = "http://localhost:3001/users/";
+    fetch(url, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify({UID:UID, email:email})
+     
+    })
+      .then(res => {
+        res.json();
+      })
+      .then(response => {
+        console.log(response);
+            })
+      .catch(error => console.error('Error:', error));
+  }
+
   handleSignUp = async event => {
     event.preventDefault();
     const { email, password } = event.target.elements;
@@ -11,8 +32,9 @@ class SignUpContainer extends Component {
       const user = await app
         .auth()
         .createUserWithEmailAndPassword(email.value, password.value);
-        console.log(user.email, user.uid);
-        this.props.history.push("/");
+      this.postUserToDB(user.UID, user.email);
+      console.log(user.email, user.uid);
+      this.props.history.push("/");
     } catch (error) {
       alert(error);
     }
