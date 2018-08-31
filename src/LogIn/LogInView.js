327f6ec3b45121b0546c833bfb7f93829ac80bd4
LoserPanda <loserpanda82@gmail.com>
2018-08-31 09:42:02 +0300

    joumaan88

--- src/LogIn/LogInView.js
+++ src/LogIn/LogInView.js
@@ -2,31 +2,25 @@ import React from "react";
 
 const LogInView = ({ onSubmit }) => {
   return (
-    <div>
+    <div className="formdiv">
       <h1>Log in</h1>
-      <form className="form-inline" onSubmit={onSubmit}>
-      <div clasNames="form-group">
+      <form onSubmit={onSubmit}>
         <label>
           Email
+          </label>
           <input
-            style={{ width: "100%" }}
             name="email"
             type="email"
             placeholder="Email"
           />
-        </label>
-        </div>
-        <div className="form-group">
         <label>
           Password
+          </label>
           <input
-            style={{ width: "100%" }}
             name="password"
             type="password"
             placeholder="Password"
           />
-        </label>
-        </div>
         <button className="btn btn-default" type="submit">Log in</button>
       </form>
     </div>
--- src/SignUp/SignUpView.js
+++ src/SignUp/SignUpView.js
@@ -2,27 +2,25 @@ import React from "react";
 
 const SignUpView = ({ onSubmit }) => {
   return (
-    <div>
+    <div className="formdiv">
       <h1>Sign up</h1>
       <form onSubmit={onSubmit}>
         <label>
           Email
-          <input
-            style={{ width: "100%" }}
-            name="email"
-            type="email"
-            placeholder="Email"
-          />
-        </label>
+          </label>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+        />
         <label>
           Password
-          <input
-            style={{ width: "100%" }}
-            name="password"
-            type="password"
-            placeholder="Password"
-          />
-        </label>
+          </label>
+        <input
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
         <button className="btn btn-default" type="submit">Sign Up</button>
       </form>
     </div>
